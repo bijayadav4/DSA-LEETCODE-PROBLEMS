14b6585c69ccaad538bc4f6328531d34fb896575
Bijay Yadav <bijayy053@gmail.com>
2026-09-14 22:07:01 +0530

    Time: 1 ms (90.08%), Space: 42.4 MB (70.02%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,15 +1,7 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-
-        while (n > 1) {
-
-            if (n % 4 != 0) {
-                return false;
-            }
-
-            n = n / 4;
-        }
-
-        return n == 1;
+        return n > 0
+            && (n & (n - 1)) == 0
+            && (n & 0x55555555) != 0;
     }
 }
