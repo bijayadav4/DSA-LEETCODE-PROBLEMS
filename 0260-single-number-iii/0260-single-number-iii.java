0ec98f57c94ad7d7a2aec847c30c8cc9e5d8d221
Bijay Yadav <bijayy053@gmail.com>
2026-09-14 23:24:35 +0530

    Time: 1 ms (100%), Space: 47.8 MB (88.15%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+
+        int xor = 0;
+
+        for (int num : nums) {
+            xor ^= num;
+        }
+
+        int rightmostBit = xor & -xor;
+
+        int a = 0;
+        int b = 0;
+
+        for (int num : nums) {
+
+            if ((num & rightmostBit) == 0) {
+                a ^= num;
+            } else {
+                b ^= num;
+            }
+        }
+
+        return new int[]{a, b};
+    }
+}
